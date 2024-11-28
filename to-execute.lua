d31d7c81ad36b88b095de503ca85d91eaf96349f
sdffsdfdssfdfsdsdf <147963347+sdffsdfdssfdfsdsdf@users.noreply.github.com>
2024-11-27 21:12:57 -0500

    Update to-execute.lua

--- to-execute.lua
+++ to-execute.lua
@@ -1,2 +1,2 @@
 _G.Key = "test"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sdffsdfdssfdfsdsdf/Lua-lister/refs/heads/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/546godly-pubert654/6H97UF8jVGz-main-Ki79UY76g/refs/heads/main/loader"))()
