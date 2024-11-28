8176db9efec456d7cb42ae89815fe41bba0a8c39
sdffsdfdssfdfsdsdf <147963347+sdffsdfdssfdfsdsdf@users.noreply.github.com>
2024-11-27 21:07:35 -0500

    Update whitelist.php

--- whitelist.php
+++ whitelist.php
@@ -2,7 +2,7 @@
 $current_data = file_get_contents("keys.json");
 $array_data = json_decode($current_data, true);
 $key = $_GET["key"];
-$encoded_key = hash("sha256", $key);
+$encoded_key = hash("1ee369af7e1f973691069bc75425bdeab8c9d3c44c7fddbed11955a67991b681", $key);
 if(in_array("$encoded_key", $array_data)){
     echo "success";
 }
